4fb4bb3d1f113b905cfe0f28323ff8c6f82f566b
Kian Leong LOH <kian_leong@live.com>
2018-12-06 12:46:30 +0800

    first commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        Hi
+      </div>
+    );
+  }
+}
+
+export default Header;
